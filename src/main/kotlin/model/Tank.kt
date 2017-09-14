8660fa6411072cfdf861032b5e97d85c1dce155b
ZhangYeSong <17602105720@163.com>
2017-09-14 23:50:30 +0800

    我方坦克移动

--- src/main/kotlin/model/Tank.kt
+++ src/main/kotlin/model/Tank.kt
@@ -4,6 +4,7 @@ import org.itheima.kotlin.game.core.Painter
 
 class Tank(override var x: Int, override var y: Int) : View {
     var direction = Directions.UP
+    var speed = 8
 
     override var width = Config.block
     override var height = Config.block
@@ -19,6 +20,21 @@ class Tank(override var x: Int, override var y: Int) : View {
     }
 
     fun move(direction: Directions) {
-        this.direction = direction
+        if (this.direction != direction) {
+            this.direction = direction
+            return
+        }
+
+        when (direction) {
+            Directions.UP -> this.y -= speed
+            Directions.DOWN -> this.y += speed
+            Directions.LEFT -> this.x -= speed
+            Directions.RIGHT -> this.x += speed
+        }
+
+        if(this.x < 0) this.x = 0
+        if(this.x > Config.gameWidth - width) this.x = Config.gameWidth - width
+        if(this.y < 0) this.y = 0
+        if(this.y > Config.gameHeight - height) this.y = Config.gameHeight - height
     }
 }
